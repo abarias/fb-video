d5cafa99d31d68bf3bd1bd6646658e51dcc4470b
AB Arias <abarias23@gmail.com>
2012-08-05 11:38:36 +0800

    added files DefaultViewController and new View controller in sotryboard

---
+++ fb-video/DefaultViewController.h
@@ -0,0 +1,14 @@
+//
+//  DefaultViewController.h
+//  fb-video
+//
+//  Created by AB Arias on 8/5/12.
+//  Copyright (c) 2012 Chevron Holdings Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DefaultViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIButton *login;
+
+@end
---
+++ fb-video/DefaultViewController.m
@@ -0,0 +1,48 @@
+//
+//  DefaultViewController.m
+//  fb-video
+//
+//  Created by AB Arias on 8/5/12.
+//  Copyright (c) 2012 Chevron Holdings Inc. All rights reserved.
+//
+
+#import "DefaultViewController.h"
+
+@interface DefaultViewController ()
+- (IBAction)login:(id)sender;
+@end
+
+@implementation DefaultViewController
+@synthesize login;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [self setLogin:nil];
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (IBAction)login:(id)sender {
+}
+@end
